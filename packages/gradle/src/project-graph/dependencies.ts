a6ea2339f3c4cb779f944b96667d162136394797
FrozenPandaz <jasonjean1993@gmail.com>
2023-08-04 14:47:42 -0400

    gradle dependencies plugin v1

--- packages/gradle/src/project-graph/dependencies.ts
+++ packages/gradle/src/project-graph/dependencies.ts
@@ -1,22 +1,34 @@
 import {
+  ProjectFileMap,
   ProjectGraph,
   ProjectGraphBuilder,
   ProjectGraphProcessorContext,
+  workspaceRoot,
 } from '@nx/devkit';
-import { execGradle } from '../utils/exec-gradle';
-import { basename, dirname } from 'node:path';
+import { execGradleAsync } from '../utils/exec-gradle';
+import { basename, dirname, relative } from 'node:path';
+import {
+  createProjectRootMappings,
+  findProjectForPath,
+  ProjectRootMappings,
+} from 'nx/src/project-graph/utils/find-project-for-path';
 
-export function processProjectGraph(
+export async function processProjectGraph(
   graph: ProjectGraph,
   context: ProjectGraphProcessorContext
 ) {
   const builder = new ProjectGraphBuilder(graph, context.fileMap);
-  const dependencies = locateDependencies(graph, context);
+  const projectRootMappings = createProjectRootMappings(graph.nodes);
+  console.time('locating gradle dependencies');
+  const dependencies = await locateDependencies(projectRootMappings, context);
+  console.timeEnd('locating gradle dependencies');
 
   for (const dep of dependencies) {
     try {
       builder.addStaticDependency(dep.source, dep.target, dep.file);
-    } catch {}
+    } catch {
+      /* noop */
+    }
   }
 
   return builder.getUpdatedProjectGraph();
@@ -24,23 +36,54 @@ export function processProjectGraph(
 
 const gradleConfigFileNames = new Set(['build.gradle', 'build.gradle.kts']);
 
-function locateDependencies(
-  graph: ProjectGraph,
-  { filesToProcess, projectsConfigurations }: ProjectGraphProcessorContext
-) {
-  const dependencies: { source: string; target: string; file: string }[] = [];
+function findGradleFiles(filesToProcess: ProjectFileMap) {
+  const gradleFiles: [string, string][] = [];
+
   for (const [source, files] of Object.entries(filesToProcess)) {
     for (const file of files) {
       if (gradleConfigFileNames.has(basename(file.file))) {
-        const projectRoot = dirname(file.file);
-        const lines = execGradle(['dependencies'], {
-          cwd: projectRoot,
-        })
+        gradleFiles.push([source, file.file]);
+      }
+    }
+  }
+  return gradleFiles;
+}
+
+async function locateDependencies(
+  projectRootMappings: ProjectRootMappings,
+  { filesToProcess }: ProjectGraphProcessorContext
+): Promise<
+  {
+    source: string;
+    target: string;
+    file: string;
+  }[]
+> {
+  const gradleFiles = findGradleFiles(filesToProcess);
+  return (
+    await Promise.all(
+      gradleFiles.map(async ([source, gradleFile]) => {
+        const projectRoot = dirname(gradleFile);
+        console.time('getting gradle dependencies for ' + projectRoot);
+        const lines = (
+          await execGradleAsync(
+            [
+              'dependencies',
+              '--configuration=implementationDependenciesMetadata',
+            ],
+            {
+              cwd: projectRoot,
+            }
+          )
+        )
           .toString()
           .split('\n');
+        console.timeEnd('getting gradle dependencies for ' + projectRoot);
         let inDeps = false;
+        const gradleProjectDependencies: string[] = [];
+        console.time('processing output for ' + projectRoot);
         for (const line of lines) {
-          if (line.startsWith('implementation ')) {
+          if (line.startsWith('implementationDependenciesMetadata')) {
             inDeps = true;
             continue;
           }
@@ -51,22 +94,68 @@ function locateDependencies(
               continue;
             }
             const [indents, dep] = line.split('--- ');
-            if (indents === '\\' && dep.startsWith('project ')) {
-              const target = dep
+            if (
+              (indents === '\\' || indents === '+') &&
+              dep.startsWith('project ')
+            ) {
+              const targetGradleProjectName = dep
                 .substring('project '.length)
                 .replace(/ \(n\)$/, '')
                 .trim();
-              console.log(source, '=>', target);
-              dependencies.push({
-                source: source,
-                target,
-                file: file.file,
-              });
+              gradleProjectDependencies.push(targetGradleProjectName);
             }
           }
         }
-      }
+        console.timeEnd('processing output for ' + projectRoot);
+
+        console.time('getting nx names for ' + projectRoot);
+        const deps = Promise.all(
+          gradleProjectDependencies.map(async (gradleProjectName) => {
+            const target = await getNxProjectName(
+              gradleProjectName,
+              projectRootMappings
+            );
+            return {
+              source: source,
+              target,
+              file: gradleFile,
+            };
+          })
+        );
+        console.timeEnd('getting nx names for ' + projectRoot);
+
+        return deps;
+      })
+    )
+  ).flat();
+}
+
+const gradleToNxProjectMap = new Map<string, string>();
+
+async function getNxProjectName(
+  gradleProjectName: string,
+  projectRootMappings: ProjectRootMappings
+): Promise<string> {
+  if (gradleToNxProjectMap.has(gradleProjectName)) {
+    return gradleToNxProjectMap.get(gradleProjectName);
+  }
+  const gradleProjectProperties = (
+    await execGradleAsync([gradleProjectName + ':properties'], {})
+  ).toString();
+  for (const line of gradleProjectProperties.split('\n')) {
+    if (line.startsWith('buildFile: ')) {
+      const buildFilePath = relative(
+        workspaceRoot,
+        line.substring('buildFile: '.length).trim()
+      );
+
+      const nxProjectName = findProjectForPath(
+        buildFilePath,
+        projectRootMappings
+      );
+      gradleToNxProjectMap.set(gradleProjectName, nxProjectName);
+
+      return nxProjectName;
     }
   }
-  return dependencies;
 }
--- packages/gradle/src/utils/exec-gradle.ts
+++ packages/gradle/src/utils/exec-gradle.ts
@@ -3,8 +3,10 @@ import { join } from 'node:path';
 import { existsSync } from 'node:fs';
 import {
   ExecFileSyncOptionsWithBufferEncoding,
+  execFile,
   execFileSync,
 } from 'node:child_process';
+import { ExecFileOptions } from 'child_process';
 
 export function execGradle(
   args: string[],
@@ -16,4 +18,35 @@ export function execGradle(
   }
 
   return execFileSync(gradleBinaryPath, args, execOptions);
+}
+
+export function execGradleAsync(
+  args: ReadonlyArray<string>,
+  execOptions: ExecFileOptions
+) {
+  const gradleBinaryPath = join(workspaceRoot, './gradlew');
+  if (!existsSync(gradleBinaryPath)) {
+    throw new Error('Gradle is not setup. Run "nx g @nx/gradle:init"');
+  }
+
+  return new Promise<Buffer>((res, rej) => {
+    const cp = execFile(gradleBinaryPath, args, execOptions);
+
+    let stdout = Buffer.from('');
+    cp.stdout.on('data', (data) => {
+      stdout += data;
+    });
+
+    cp.on('exit', (code) => {
+      if (code === 0) {
+        res(stdout);
+      } else {
+        rej(
+          new Error(
+            `Executing Gradle with ${args.join(' ')} failed with code: ${code}`
+          )
+        );
+      }
+    });
+  });
 }
