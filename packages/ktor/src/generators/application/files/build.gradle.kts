6821ae1c8137d9aa2be104d186f27a10d36dc0bd
FrozenPandaz <jasonjean1993@gmail.com>
2023-08-04 11:18:04 -0400

    add serialization to ktor app

--- packages/ktor/src/generators/application/files/build.gradle.kts
+++ packages/ktor/src/generators/application/files/build.gradle.kts
@@ -25,6 +25,8 @@ repositories {
 dependencies {
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
