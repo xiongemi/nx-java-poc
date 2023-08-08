c7cfa4c7ec408de42a69e39ccaa0efdb76282ba3
FrozenPandaz <jasonjean1993@gmail.com>
2023-08-08 11:12:58 -0400

    add ktor module as ktor lib

--- packages/ktor/src/generators/library/files/build.gradle.kts
+++ packages/ktor/src/generators/library/files/build.gradle.kts
@@ -6,9 +6,6 @@ val logback_version: String by project
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
     id("org.jetbrains.kotlin.jvm") version "1.8.20"
-
-    // Apply the java-library plugin for API and implementation separation.
-    `java-library`
 }
 
 group = "<%= sourcePackage %>"
@@ -20,6 +17,10 @@ repositories {
 }
 
 dependencies {
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+
+    testImplementation("io.ktor:ktor-server-test-host:$ktor_version")
+
     // Use the Kotlin JUnit 5 integration.
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 
@@ -27,12 +28,6 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.2")
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
-    // This dependency is exported to consumers, that is to say found on their compile classpath.
-    api("org.apache.commons:commons-math3:3.6.1")
-
-    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-    implementation("com.google.guava:guava:31.1-jre")
 }
 
 tasks.named<Test>("test") {
